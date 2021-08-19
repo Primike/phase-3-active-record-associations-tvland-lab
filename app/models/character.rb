a784719d8b3a2919e7df1ec57d32f2ebcc490860
Prince <avecillasprince@yahoo.com>
2021-08-19 19:31:29 -0400

    commit tree 95d650eadbf1a584cff17eaf1c2ef2bcc4729790

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :show
+    belongs_to :actor
+
+    def say_that_thing_you_say
+        x = self.catchphrase
+        "#{self.name} always says: #{x}"
+    end
 end
